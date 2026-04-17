61c94ccdbf0b632272d53076358c532248460d1b
Luciano Scialchi <63683848+LucianoS01@users.noreply.github.com>
2026-04-17 19:53:41 -0300

    Sacamos parte del backoofice

--- Frontend-Depfund/src/App.tsx
+++ Frontend-Depfund/src/App.tsx
@@ -6,12 +6,12 @@ import ForgotPassword from "./features/auth.login/components/ForgotPassword";
 import EditProfile from "./features/auth.login/components/EditProfile";
 import ProfileView from "./features/auth.login/components/ProfileView";
 import Dashboard from "./features/auth.login/components/Dashboard";
-import RolesManager from "./features/auth.login/components/RolesManager";
-import RoleEdit from "./features/auth.login/components/RoleEdit";
-import PermiseManager from "./features/auth.login/components/PermiseManager";
-import PermiseEdit from "./features/auth.login/components/PermiseEdit";
-import Users from "./features/auth.login/components/Users";
-import UserRolEdit from "./features/auth.login/components/UserRolEdit";
+// import RolesManager from "./features/auth.login/components/RolesManager";
+// import RoleEdit from "./features/auth.login/components/RoleEdit";
+// import PermiseManager from "./features/auth.login/components/PermiseManager";
+// import PermiseEdit from "./features/auth.login/components/PermiseEdit";
+// import Users from "./features/auth.login/components/Users";
+// import UserRolEdit from "./features/auth.login/components/UserRolEdit";
 
 
 const App: React.FC = () => {
@@ -26,13 +26,13 @@ const App: React.FC = () => {
       <Route path="/edit-profile" element={<EditProfile />} />
       <Route path="/ProfileView" element={<ProfileView />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/RolesManager" element={<RolesManager />} />
+      {/* <Route path="/RolesManager" element={<RolesManager />} />
       <Route path="/roles" element={<RolesManager />} />
       <Route path="/roles/edit/:id" element={<RoleEdit />} />
       <Route path="/permisos" element={<PermiseManager />} />
       <Route path="/permisos/edit/:id" element={<PermiseEdit />} />
       <Route path="/users" element={<Users />} />
-<Route path="/users/edit-role/:id" element={<UserRolEdit />} />
+<Route path="/users/edit-role/:id" element={<UserRolEdit />} /> */}
     </Routes>
   );
 };
